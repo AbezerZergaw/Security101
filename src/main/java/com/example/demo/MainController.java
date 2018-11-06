8cf2b68255b37ff686eb28a901ba412ee05441a2
GBTC440013UR <abezes.haile@gmail.com>
2018-11-06 14:57:27 -0500

    updated

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.security.Principal;
 
 @Controller
@@ -36,11 +37,28 @@ public class MainController {
         return "secure";
     }
 
-    @RequestMapping(value="/register", method = RequestMethod.GET)
-    public String processRegistrationPage(Model model){
+   @GetMapping("/register")
+    public String showRegistrationPage(Model model){
         model.addAttribute("user", new User());
         return "registration";
     }
+
+    @PostMapping("/register")
+    public String processRegistrationPage(@Valid @ModelAttribute("user") User user, BindingResult result, Model model){
+
+        model.addAttribute("user", user);
+        if(result.hasErrors()){
+
+            return "registration";
+        }
+        else {
+
+            userService.saveUser(user);
+            model.addAttribute("message", "User Account Created");
+        }
+        return "index";
+    }
+
 
 }
 
--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -42,7 +42,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/","/h2/**").permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll()
+        http.authorizeRequests().antMatchers("/","/h2/**","/register").permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll()
                 .and().logout().logoutRequestMatcher( new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").permitAll().permitAll().and().httpBasic();
 
         http.csrf().disable();
